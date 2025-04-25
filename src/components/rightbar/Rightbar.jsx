2aeaafd106a47bc858230d29ba71ebaaab573e11
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-25 23:35:49 +0530

    format fixes

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,13 +1,18 @@
-import './rightbar.css'
+import "./rightbar.css";
 
-export default function Rightbar(){
-    return <div className='rightbar'>
-        <div className="rightbarWrapper">
-            <div className="birthdayContainer">
-                <img src='assets/gift.png' alt='birthday' className='birthdayImg' />
-                <span className='birthdayText'>Mahatma gandhi and 3 other are celebrating their birthdays today, wish em!</span>
-            </div>
-            <img className='rightbarAd' src='/assets/ad.png' alt='ad' />
+export default function Rightbar() {
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="assets/gift.png" alt="birthday" className="birthdayImg" />
+          <span className="birthdayText">
+            Mahatma gandhi and 3 other are celebrating their birthdays today,
+            wish em!
+          </span>
         </div>
+        <img className="rightbarAd" src="/assets/ad.png" alt="ad" />
+      </div>
     </div>
+  );
 }
