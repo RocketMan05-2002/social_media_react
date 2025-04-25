7cee22a3b31e961a5fb91904d1b632204f197217
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-25 23:35:31 +0530

    format fixes

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -5,7 +5,7 @@ export default function Rightbar(){
         <div className="rightbarWrapper">
             <div className="birthdayContainer">
                 <img src='assets/gift.png' alt='birthday' className='birthdayImg' />
-                <span className='birthdayText'>rahul gandhi and 3 other are celebrating their birthdays today, wish em!</span>
+                <span className='birthdayText'>Mahatma gandhi and 3 other are celebrating their birthdays today, wish em!</span>
             </div>
             <img className='rightbarAd' src='/assets/ad.png' alt='ad' />
         </div>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,57 +1,77 @@
-import React, { useState } from 'react'
-import './sidebar.css'
-import { sidebarLinks,friends } from '../../utils/utils'
+import React, { useState } from "react";
+import "./sidebar.css";
+import { sidebarLinks, friends } from "../../utils/utils";
+import { blue } from "@mui/material/colors";
 
 const Sidebar = () => {
-  const[showMoreToggle,setShowMoreToggle] = useState(false);
-  const[showMoreToggle2,setShowMoreToggle2] = useState(false);
+  const [showMoreToggle, setShowMoreToggle] = useState(false);
+  const [showMoreToggle2, setShowMoreToggle2] = useState(false);
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       <div className="sidebarWrapper">
         <ul className="sidebarList">
-            {
-              showMoreToggle 
-              ? sidebarLinks.map((item)=>{
-                return <li key={item.id} className='sidebarListItem'>
-                  {item.icon}
-                  <span className='sidebarListItemText'>{item.name}</span>
-                </li>
+          {showMoreToggle
+            ? sidebarLinks.map((item) => {
+                return (
+                  <li key={item.id} className="sidebarListItem">
+                    {item.icon}
+                    <span className="sidebarListItemText">{item.name}</span>
+                  </li>
+                );
               })
-              : sidebarLinks.slice(0,-4).map((item)=>{
-                return <li key={item.id} className='sidebarListItem'>
-                  {item.icon}
-                  <span className='sidebarListItemText'>{item.name}</span>
-                </li>
-              })
-            }
+            : sidebarLinks.slice(0, -4).map((item) => {
+                return (
+                  <li key={item.id} className="sidebarListItem">
+                    {item.icon}
+                    <span className="sidebarListItemText">{item.name}</span>
+                  </li>
+                );
+              })}
         </ul>
-        <button className='sidebarButton' onClick={()=>setShowMoreToggle(!showMoreToggle)}>
-          {showMoreToggle?"Show Less":"Show More"}
+        <button
+          className="sidebarButton"
+          onClick={() => setShowMoreToggle(!showMoreToggle)}
+        >
+          {showMoreToggle ? "Show Less" : "Show More"}
         </button>
-        <hr className='sidebarHr' />
-        <ul className='sidebarFriendList'>
-          {
-            showMoreToggle2
-            ? friends.map((item)=>{
-              return <li key={item.id} className='sidebarFriend'>
-                <img src={item.profileSrc} alt={item.name} className='sidebarFriendImg' />
-                <span className="sidebarFriendName">{item.name}</span> 
-              </li>
-            })
-            : friends.slice(0,-5).map((item)=>{
-              return <li key={item.id} className='sidebarFriend'>
-                <img src={item.profileSrc} alt={item.name} className='sidebarFriendImg' />
-                <span className="sidebarFriendName">{item.name}</span> 
-              </li>
-            })
-          }
+        <hr className="sidebarHr" />
+        <h3 style={{ marginBottom: 10, color: "#1877f2" }}>Friends</h3>
+        <ul className="sidebarFriendList">
+          {showMoreToggle2
+            ? friends.map((item) => {
+                return (
+                  <li key={item.id} className="sidebarFriend">
+                    <img
+                      src={item.profileSrc}
+                      alt={item.name}
+                      className="sidebarFriendImg"
+                    />
+                    <span className="sidebarFriendName">{item.name}</span>
+                  </li>
+                );
+              })
+            : friends.slice(0, -5).map((item) => {
+                return (
+                  <li key={item.id} className="sidebarFriend">
+                    <img
+                      src={item.profileSrc}
+                      alt={item.name}
+                      className="sidebarFriendImg"
+                    />
+                    <span className="sidebarFriendName">{item.name}</span>
+                  </li>
+                );
+              })}
         </ul>
-        <button className='sidebarButton' onClick={()=>setShowMoreToggle2(!showMoreToggle2)}>
-          {showMoreToggle2 ?"Show Less":"Show More"}
+        <button
+          className="sidebarButton"
+          onClick={() => setShowMoreToggle2(!showMoreToggle2)}
+        >
+          {showMoreToggle2 ? "Show Less" : "Show More"}
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
