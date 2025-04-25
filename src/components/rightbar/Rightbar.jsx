3c707e9efc6c2f8e23da674a065de5b1af26e4ea
Rocketman <abhinayshambharkar1@gmail.com>
2025-04-25 19:18:27 +0530

    add rightbar birthday section and advertisement menu

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -2,6 +2,12 @@ import './rightbar.css'
 
 export default function Rightbar(){
     return <div className='rightbar'>
-        Rightbar
+        <div className="rightbarWrapper">
+            <div className="birthdayContainer">
+                <img src='assets/gift.png' alt='birthday' className='birthdayImg' />
+                <span className='birthdayText'>rahul gandhi and 3 other are celebrating their birthdays today, wish em!</span>
+            </div>
+            <img className='rightbarAd' src='/assets/ad.png' alt='ad' />
+        </div>
     </div>
 }
